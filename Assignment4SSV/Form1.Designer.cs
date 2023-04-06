153322ca86b6452bf053601271b37d115c2b0fae
Latitude E6430 <Latitude E6430@DESKTOP-G2F3DCJ>
2023-04-06 00:55:51 -0400

    Add project files.

---
+++ Assignment4SSV/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace Assignment4SSV
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtMonthlyPayment = new System.Windows.Forms.TextBox();
+            this.txtPurchasePrice = new System.Windows.Forms.TextBox();
+            this.txtAmountToFinance = new System.Windows.Forms.TextBox();
+            this.txtLoanTerm = new System.Windows.Forms.TextBox();
+            this.txtInterestRate = new System.Windows.Forms.TextBox();
+            this.txtDownPaymentAmount = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtMonthlyPayment
+            // 
+            this.txtMonthlyPayment.Location = new System.Drawing.Point(459, 93);
+            this.txtMonthlyPayment.Name = "txtMonthlyPayment";
+            this.txtMonthlyPayment.Size = new System.Drawing.Size(100, 20);
+            this.txtMonthlyPayment.TabIndex = 0;
+            // 
+            // txtPurchasePrice
+            // 
+            this.txtPurchasePrice.Location = new System.Drawing.Point(152, 28);
+            this.txtPurchasePrice.Name = "txtPurchasePrice";
+            this.txtPurchasePrice.Size = new System.Drawing.Size(100, 20);
+            this.txtPurchasePrice.TabIndex = 1;
+            this.txtPurchasePrice.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // txtAmountToFinance
+            // 
+            this.txtAmountToFinance.Location = new System.Drawing.Point(459, 28);
+            this.txtAmountToFinance.Name = "txtAmountToFinance";
+            this.txtAmountToFinance.Size = new System.Drawing.Size(100, 20);
+            this.txtAmountToFinance.TabIndex = 2;
+            // 
+            // txtLoanTerm
+            // 
+            this.txtLoanTerm.Location = new System.Drawing.Point(152, 233);
+            this.txtLoanTerm.Name = "txtLoanTerm";
+            this.txtLoanTerm.Size = new System.Drawing.Size(100, 20);
+            this.txtLoanTerm.TabIndex = 3;
+            // 
+            // txtInterestRate
+            // 
+            this.txtInterestRate.Location = new System.Drawing.Point(152, 160);
+            this.txtInterestRate.Name = "txtInterestRate";
+            this.txtInterestRate.Size = new System.Drawing.Size(100, 20);
+            this.txtInterestRate.TabIndex = 4;
+            // 
+            // txtDownPaymentAmount
+            // 
+            this.txtDownPaymentAmount.Location = new System.Drawing.Point(152, 89);
+            this.txtDownPaymentAmount.Name = "txtDownPaymentAmount";
+            this.txtDownPaymentAmount.Size = new System.Drawing.Size(100, 20);
+            this.txtDownPaymentAmount.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(64, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Purchase Price:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(121, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Down Payment Amount:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 167);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Interest Rate (annual):";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(42, 240);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(104, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Loan Term (months):";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(354, 31);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(99, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Amount to Finance:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(362, 96);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(91, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Monthly Payment:";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(32, 287);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 12;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(484, 286);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 13;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(594, 322);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtDownPaymentAmount);
+            this.Controls.Add(this.txtInterestRate);
+            this.Controls.Add(this.txtLoanTerm);
+            this.Controls.Add(this.txtAmountToFinance);
+            this.Controls.Add(this.txtPurchasePrice);
+            this.Controls.Add(this.txtMonthlyPayment);
+            this.Name = "Form1";
+            this.Text = "Monthly Payment Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtMonthlyPayment;
+        private System.Windows.Forms.TextBox txtPurchasePrice;
+        private System.Windows.Forms.TextBox txtAmountToFinance;
+        private System.Windows.Forms.TextBox txtLoanTerm;
+        private System.Windows.Forms.TextBox txtInterestRate;
+        private System.Windows.Forms.TextBox txtDownPaymentAmount;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
+
---
+++ Assignment4SSV/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Assignment4SSV
+{ /* Name: Snehal Vibhute
+   * Date: 3/1/2023
+   * Pupose: Create a desktop application that will allow user to calculate the monthly payment for a fixed-rate interest loan
+   */
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            // Validate required fields are not empty
+            if(string.IsNullOrWhiteSpace(txtPurchasePrice.Text) ||
+                string.IsNullOrWhiteSpace(txtDownPaymentAmount.Text) ||
+                string.IsNullOrWhiteSpace(txtInterestRate.Text) ||
+                string.IsNullOrWhiteSpace(txtLoanTerm.Text))
+            { MessageBox.Show("This inputs form cannot be empty.", "Missing Required Fields", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // validate numeric input for required field
+            double purchasePrice, downPaymentAmount, interestRate;
+            int loanTerm;
+
+            if(!double.TryParse(txtPurchasePrice.Text, out purchasePrice))
+            {
+                MessageBox.Show("Please enter a valid numeric value for Purchase Price.","Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return; 
+            }
+
+            if (!double.TryParse(txtDownPaymentAmount.Text, out downPaymentAmount))
+            {
+                MessageBox.Show("Please enter a valid numeric value for Down Payment Amount.", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            
+            if (!double.TryParse(txtInterestRate.Text, out interestRate))
+            {
+                MessageBox.Show("This input's value must be a valid decimal.", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (!int.TryParse(txtLoanTerm.Text, out loanTerm))
+            {
+                MessageBox.Show("Please enter a valid numeric value for Loan Term(in months).", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Calculate amount to finance and monthly payment
+            double amountToFinance = purchasePrice - downPaymentAmount;
+            double monthlyInterestRate = interestRate / 1200;
+            double monthlyPayment = (amountToFinance * monthlyInterestRate) * Math.Pow(1 + monthlyInterestRate, loanTerm) / (Math.Pow(1 + monthlyInterestRate, loanTerm) - 1);
+
+            // Display results
+            txtAmountToFinance.Text = amountToFinance.ToString("C");
+            txtMonthlyPayment.Text = monthlyPayment.ToString("C");
+            }
+        }
+    }
+
+
+
